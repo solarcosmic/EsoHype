a16156c62c9cd2394848c86799609e70f7fb8fe6
Solar <abe@solarcosmic.net>
2025-07-06 20:29:23 +1200

    Repeat Blocks (v1.0.0)

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # EsoHype
 An esoteric programming language written in Lua where you have to be nice.
 
+## Manners
+It is required to have `please` or `pls` where it is required on the line, otherwise the interpreter will refuse to parse it. For example:
+```py
+can my_string be "Works!" please  # Will work
+display my_string pls
+
+can another_string be "Uh oh!"    # Won't work
+display another_string pls
+```
+
+## Displays
+To print out strings, numbers, or variables, you can use `display`. This function only contains one argument, which is what you want to print out. For example:
+```py
+display 5 pls               # 5
+display 2 * 7 pls          # 14
+display "this works!" pls  # this works!
+```
+Variables can also be used.
+```py
+can x be 27 pls
+display x pls              # 27
+```
+However, the following example below will not work:
+```
+display 5
+```
+This is because it is missing the `please` or `pls` keyword at the end of the line.
+
 ## Variables
 To define variables, you need to ask the interpreter to define it, for example:
 ```py
@@ -11,23 +39,65 @@ where `x` is the variable name and `5` is the value you want to set it to, for e
 Variables can be used almost anywhere, for example, in `display` statements:
 ```py
 can y be 27 pls
-display y # Outputs 27
+display y pls # Outputs 27
 ```
 
 They can also contain strings:
 ```py
 can y be "this works!" pls
-display y
+display y pls
 ```
 as well as basic arithmetic:
 ```py
 can z be 5 * (2 + 5) pls
-display z
+display z pls
 ```
-and they also can be reassigned.
+and they can also be mutable (reassigned).
 ```py
 can x be 27 / 2 pls
-display x
+display x pls # 13.5
 can x be 5 * 5 pls
-display x
+display x pls # 25
 ```
+
+NOTE: As of version v1.0.0, arithmetic may only be done in variables.
+
+## Repeat Loops
+```py
+can x be 25 pls
+repeat x times (pls)
+    display "This will happen 25 times!" pls
+endrepeat (pls)
+```
+Repeat loops are blocks of code that repeat a certain amount. They start with `repeat x times` where `x` is the number or variable, and end with `endrepeat`.
+
+You may set the repeat count to a number:
+```py
+repeat 5 times pls
+    display "Repeated 5 times!" pls
+endrepeat pls
+```
+Or you may also set it to be a numeric variable:
+```py
+can count be 7 pls
+repeat count times pls
+    display "Repeated 7 times!" pls
+endrepeat pls
+```
+If you do set the count to be a numeric variable, it is possible to do this:
+```py
+can x be 5 pls
+repeat x times
+    can x be x + 1 pls
+    display x pls
+endrepeat pls
+```
+However, instead of running indefinitely until stopped, EsoHype will only grab the count provided when the repeat block is initialised (or initialized), resulting in this output below:
+```
+6
+7
+8
+9
+10
+```
+Indents are not needed inside a repeat block as they will be ignored by the interpreter, but they make the code easier to read.
--- esohype.lua
+++ esohype.lua
@@ -1,4 +1,4 @@
-doc = io.open("example.hyp", "r")
+local doc = io.open("example.hyp", "r")
 io.input(doc)
 
 local variables = {}
@@ -48,6 +48,17 @@ end
 
 function eval_display(tokens)
     local rawValue = tokens[2]
+    local please = tokens[3]
+
+    if please ~= nil then
+        if please:lower() ~= "please" and please:lower() ~= "pls" then
+            print(" === Error on line "..line_count..": Not parsing this. Please mind your manners next time.")
+            return
+        end
+    else
+        print(" === Error on line "..line_count..": Not parsing this. Please mind your manners next time.")
+        return
+    end
 
     if rawValue:sub(1,1) == "\"" and rawValue:sub(-1) == "\"" then
         print(rawValue:sub(2,-2))
@@ -57,10 +68,81 @@ function eval_display(tokens)
             print(numValue)
         else
             if variables[rawValue] == nil then
-                print(" === Variable '"..varName.."' does not exist!")
+                print(" === Variable '"..rawValue.."' does not exist!")
             else
                 print(variables[rawValue])
             end
+        end
+    end
+end
+
+-- https://stackoverflow.com/questions/17987618/how-to-add-a-sleep-or-wait-to-my-lua-script
+function sleep(a) 
+    local sec = tonumber(os.clock() + a); 
+    while (os.clock() < sec) do end
+end
+
+function eval_wait(tokens)
+    local wait = tokens[1]
+    local seconds = tokens[3]
+
+    if wait ~= nil and wait:lower() == "wait" and seconds:lower() == "seconds" then
+        if tokens[4] == nil then
+            print(" === Error on line "..line_count..": Not parsing this. Please mind your manners next time.")
+            return
+        end
+        if tokens[4]:lower() ~= "pls" and tokens[4]:lower() ~= "please" then
+            print(" === Error on line "..line_count..": Not parsing this. Please mind your manners next time.")
+            return
+        end
+        if tonumber(tokens[2]) then
+            sleep(tonumber(tokens[2]))
+        else
+            local varName = tokens[2]
+            if variables[varName] == nil then
+                print(" === Variable '"..rawValue.."' does not exist!")
+                return
+            end
+            local varValue = variables[varName]
+            local numValue = tonumber(varValue)
+            if numValue then
+                sleep(numValue)
+            else
+                print(" === Variable '"..varName.."' is not a qualified number!")
+            end
+        end
+    end
+end
+
+function eval_repeat(tokens)
+    local count = tonumber(tokens[2])
+    if not count then
+        if variables[tokens[2]] == nil then
+            print(" === Variable '"..tokens[2].."' does not exist! EsoHype will pretend the loop isn't there and continue.")
+            return
+        end
+        local varValue = variables[tokens[2]]
+        local numValue = tonumber(varValue)
+        if numValue then
+            count = numValue
+        else
+            print(" === Variable '"..tokens[2].."' is not a qualified number!")
+        end
+    end
+    
+    local block_lines = {}
+    for extra_line in io.lines() do
+        local extra_tokens = loopTokens(extra_line)
+        if extra_tokens[1]:lower() == "endrepeat" then
+            break
+        else
+            table.insert(block_lines, extra_line)
+        end
+    end
+
+    for i = 1, count do
+        for _, block_line in ipairs(block_lines) do
+            processLine(block_line)
         end
     end
 end
@@ -73,24 +155,43 @@ function loopTokens(line)
     return tokens
 end
 
+local lineTypes = {
+    ["variable"] = eval_var,
+    ["display"] = eval_display,
+    ["wait"] = eval_wait,
+    ["repeat"] = eval_repeat
+}
+
+function processLine(line)
+    local tokens = loopTokens(line)
+    local lineType = getLineType(tokens)
+    if lineType then
+        local types = lineTypes[lineType]
+        if types then
+            types(tokens)
+        else
+            print(" === Error: Unknown line type while parsing: "..lineType)
+        end
+    end
+end
+
 function getLineType(tokens)
     if tokens[1]:lower() == "can" and tokens[3]:lower() == "be" then
         return "variable"
     elseif tokens[1]:lower() == "display" then
         return "display" -- equivalent of print
+    elseif tokens[1]:lower() == "wait" and tokens[3]:lower() == "seconds" then
+        return "wait" -- equivalent of print
+    elseif tokens[1]:lower() == "repeat" then
+        return "repeat"
     end
 end
 
 for line in io.lines() do
-    local tokens = loopTokens(line)
-    local lineType = getLineType(tokens)
-    if lineType == "variable" then
-        eval_var(tokens)
-    elseif lineType == "display" then
-        eval_display(tokens)
-    end
+    processLine(line)
     line_count = line_count + 1
 end
 --for _, varName in ipairs(variable_order) do
 --    print(varName, variables[varName])
 --end
+doc:close()
